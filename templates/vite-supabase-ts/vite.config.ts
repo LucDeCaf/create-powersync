10d6b74281c163748d3e5bb6a256ea55454235f6
Luc de Cafmeyer <ldecafmeyer@gmail.com>
2025-02-10 18:20:18 +0200

    add vite plugins wasm topLevelAwait

--- templates/vite-supabase-ts/vite.config.ts
+++ templates/vite-supabase-ts/vite.config.ts
@@ -1,4 +1,16 @@
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({ plugins: [tailwindcss()] });
+export default defineConfig({
+    optimizeDeps: {
+        include: ['@powersync/web > js-logger'],
+        exclude: ['@journeyapps/wa-sqlite', '@powersync/web'],
+    },
+    plugins: [wasm(), topLevelAwait(), tailwindcss()],
+    worker: {
+        format: 'es',
+        plugins: () => [wasm(), topLevelAwait()],
+    },
+});
