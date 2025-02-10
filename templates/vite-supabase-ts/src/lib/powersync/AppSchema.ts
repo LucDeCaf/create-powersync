6ccc557e43ffce5f8685e109e56323a3a5735fad
Luc de Cafmeyer <ldecafmeyer@gmail.com>
2025-02-10 19:52:54 +0200

    remove todolist-demo-specific AppSchema data

--- templates/vite-supabase-ts/src/lib/powersync/AppSchema.ts
+++ templates/vite-supabase-ts/src/lib/powersync/AppSchema.ts
@@ -1,33 +1,7 @@
-import { column, Schema, Table } from '@powersync/web';
+// ? Replace this file with your AppSchema.ts
 
-const todos = new Table(
-    {
-        list_id: column.text,
-        created_at: column.text,
-        completed_at: column.text,
-        description: column.text,
-        created_by: column.text,
-        completed_by: column.text,
-        completed: column.integer,
-    },
-    { indexes: { list: ['list_id'] } },
-);
+import { Schema } from '@powersync/web';
 
-const lists = new Table({
-    created_at: column.text,
-    name: column.text,
-    owner_id: column.text,
-});
-
-export const AppSchema = new Schema({
-    todos,
-    lists,
-});
-
-export const TODOS_TABLE = 'todos';
-export const LISTS_TABLE = 'lists';
+export const AppSchema = new Schema({});
 
 export type Database = (typeof AppSchema)['types'];
-
-export type TodoRecord = Database['todos'];
-export type ListRecord = Database['lists'];
